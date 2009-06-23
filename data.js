6c65440bfa0bf2ed049c8bb4218fea73a2fac919
Alex Trujillo <cryptoquick@gmail.com>
2009-06-23 05:31:36 -0600

    Numerous changes. Still working on the parallel loader.

---
+++ data.js
@@ -0,0 +1,14 @@
+/* Graphics */
+var ship_size = 48;
+var glow_size = 72;
+
+/* Ship Resources */
+
+var shipData = {
+	starbridge: {framesX: 6, framesY: 6, baseSizeX: 48, baseSizeY: 48, glowSizeX: 72, glowSizeY: 72}
+};
+
+// name, direction, speed, xstep, ystep, alpha
+var shipSystem = [
+	['starbridge', 0, 0, 0, 0, 0.0]
+];
--- ev.js
+++ ev.js
@@ -1,14 +1,42 @@
 /* Main Function */
 
 // I need to redesign this as a parallel loader function.
+var loadingGraphics = true;
+var numGraphicsLoaded = 0;
+
+var shipSheet = new Image();
+var glowSheet = new Image();
+
+var shipGraphics = [
+	[shipSheet, 'graphics/starbridge-alpha.png', false],
+	[glowSheet, 'graphics/starbridge-glow-alpha.png', false]
+];
+
+function graphicsLoaded () {
+	numGraphicsLoaded++;
+	if (numGraphicsLoaded == shipGraphics.length) {
+		loadingGraphics = false;
+	}
+}
+
 window.addEventListener('load', function () {
+	for (i = 0; i < shipGraphics.length; i++) {
+		shipGraphics[i][0].src = shipGraphics[i][1];
+		shipGraphics[i][0].onLoad = graphicsLoaded;
+	}
+	
+	while (loadingGraphics) {
+		graphicsLoaded ();
+		var canvas = document.getElementById('starbridge');
+		canvas.setAttribute('style', 'background: none');
+		Init();
+	}
+	
 	shipSheet.src = 'graphics/starbridge-alpha.png';
 	shipSheet.onload = function () {
 		glowSheet.src = 'graphics/starbridge-glow-alpha.png';
 		glowSheet.onload = function () {
-			var canvas = document.getElementById('starbridge');
-			canvas.setAttribute('style', 'background: none');
-			Init();
+			
 		}
 	}
 }, false);
@@ -23,88 +51,150 @@ function Init () {
 	SetContext();
 	
 	// Draw first frame.
-	DrawSprite(shipSheet, 0, 0, 48, 12, false);
+	DrawSprite(shipSheet, 0, 0, 48, 12, 10, false);
 }
 
-/* Input */
+/* Player Input */
 
 var smoothturn;
-var keyPressed = false;
+var smoothGo;
+var keyPressed = 0;
+var keyPressed2 = 0;
 
 function Key (evt) {
 	var currentKey = 0;
 	if (evt.type == "keydown") {
-		//some browsers support evt.charCode, some only evt.keyCode
+		// Some browsers support evt.charCode, some only evt.keyCode
 		if (evt.charCode) {
-			var charCode = evt.charCode;
+			charCode = evt.charCode;
 		}
 		else {
-			var charCode = evt.keyCode;
+			charCode = evt.keyCode;
 		}
 		// Left arrow key
 		if (charCode == 39) {
 			// setInterval animates (read, repeats) the function at a certain speed.
 			// Current speed is set to 0.03 seconds, or 33FPS.
-			if (keyPressed == false) {
-				smoothturn = window.setInterval("RotateShip('right')", 30);
+			if (keyPressed == 0) {
+				smoothturn = window.setInterval("updateShip('turnRight')", 30);
+			}
+			// If this key is pressed while the other key is active, then turn this way
+			else if (keyPressed == 37) {
+				window.clearInterval(smoothturn);
+				smoothturn = window.setInterval("updateShip('turnRight')", 30);
 			}
 			// This variable helps under the condition that if the key is held long enough
 			// to repeat, the program won't set an interval again until it is released.
-			keyPressed = true;
+			keyPressed = 39;
 		}
 		// Right arrow key
 		if (charCode == 37) {
-			if (keyPressed == false) {
-				smoothturn = window.setInterval("RotateShip('left')", 30);
+			if (keyPressed === 0) {
+				smoothturn = window.setInterval("updateShip('turnLeft')", 30);
 			}
-			keyPressed = true;
+			else if (keyPressed == 39) {
+				window.clearInterval(smoothturn);
+				smoothturn = window.setInterval("updateShip('turnLeft')", 30);
+			}
+			keyPressed = 37;
+		}
+		// Up arrow key
+		if (charCode == 38) {
+			smoothGo = window.setInterval("updateShip('speedUp')", 100);
+		}
+		// Down arrow key
+		if (charCode == 40) {
+			// This will eventually be where I'll put the 'reverse direction' code.
+		//	smoothGo = window.setInterval("updateShip('slowDown')", 100);
 		}
 	}
 	
 	if (evt.type == "keyup") {
 		window.clearInterval(smoothturn);
-		keyPressed = false;
+		window.clearInterval(smoothGo);
+		keyPressed = 0;
+	}
+}
+
+glowFlicker = 0;
+
+// Updates the ship's appearance according to its direction and speed
+function updateShip (change) {
+	frame = {x: shipSystem[0][3], y: shipSystem[0][4]};
+	alpha = shipSystem[0][5];
+	
+	if (change == 'turnRight') {
+		// Turn the ship using the stored graphical framestep
+		frame = rotateShip('right', frame);
+		recordShip (frame, false);
+	} else if (change == 'turnLeft') {
+		frame = rotateShip('left', frame);
+		recordShip (frame, false);
+	} else if (change == 'speedUp') {
+		if (alpha < 7) {
+			alpha++;
+			recordShip(false, alpha);
+		}
+		
+		if (alpha == 7) {
+			
+		}
+	} else if (change == 'slowDown') {
+		if (alpha > 0) {
+			alpha--;
+			recordShip(false, alpha);
+		}
+	}
+	
+	DrawSprite(glowSheet, frame.x, frame.y, 72, 0, alpha, true);
+	DrawSprite(shipSheet, frame.x, frame.y, 48, 12, 10, false);
+}
+
+
+
+// Update ship's stored variables
+function recordShip (frame, alpha) {
+	if (frame !== false) {
+		shipSystem[0][3] = frame.x;
+		shipSystem[0][4] = frame.y;
+	}
+	if (alpha !== false) {
+		shipSystem[0][5] = alpha;
 	}
 }
 
 /* Ship Navigation */
 
-function RotateShip (direction) {
-    // Translates across the 6x6 ship grid in either direction
+// Translates across the 6x6 ship sprite sheet in either direction
+// When it gets to the side of the sheet, go to the next row or column
+
+function rotateShip (direction, frame) {
 	if (direction == 'right') {
-		xstep++;
-		if (xstep == 6) {
-			ystep++;
-			xstep = 0;
+		frame.x++;
+		if (frame.x == 6) {
+			frame.y++;
+			frame.x = 0;
 		}
-		if (ystep == 6) {
-			ystep = 0;
+		if (frame.y == 6) {
+			frame.y = 0;
 		}
-	} else { //(direction = 'left') 
-		xstep--;
-		if (xstep < 0) {
-			ystep--;
-			xstep = 5;
+	} else if (direction == 'left') {
+		frame.x--;
+		if (frame.x < 0) {
+			frame.y--;
+			frame.x = 5;
 		}
-		if (ystep < 0) {
-			ystep = 5;
+		if (frame.y < 0) {
+			frame.y = 5;
 		}
 	}
 	
-	DrawSprite(glowSheet, xstep, ystep, 72, 0, true);
-	DrawSprite(shipSheet, xstep, ystep, 48, 12, false);
+	return {x: frame.x, y: frame.y};
 }
 
 /* Canvas Graphics Functions */
 
 // Global variables to keep track of ship orientation.
-var ship_size = 48;
-var glow_size = 72;
-var xstep = 0;
-var ystep = 0;
-
-var shipSheet = new Image();
-var glowSheet = new Image();
 
 var context;
 
@@ -117,12 +207,13 @@ function SetContext () {
 
 // Parameters: Sprite sheet (ship, glow), step of the animation
 // (which sprite on the sheet to display, x/y), how far away from
-// top/left do we want to offset the sprite, and should we clear
-// the canvas on the next frame load (true/false).
-function DrawSprite (sheet, xstep, ystep, size, offset, clear) {
+// top/left do we want to offset the sprite, transparency, and
+// should we clear the canvas on the next frame load (true/false).
+function DrawSprite (sheet, xstep, ystep, size, offset, alpha, clear) {
 	if (clear) {
 		context.clearRect (0, 0, size, size);
 	}
+	context.globalAlpha = alpha / 10;
 	context.globalCompositeOperation = 'lighter';
 	// Uses the sprite sheet to draw on the board:
 	context.drawImage(sheet,
